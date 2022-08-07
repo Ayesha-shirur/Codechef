9d9843a05c903f31fba95bc6e47e4a1aae913f25
Ayesha-shirur <64835413+Ayesha-shirur@users.noreply.github.com>
2022-08-07 20:43:07 +0530

    Update Discount.java
    Time Complexity = O(1) 
    Due to the fact that we are doing operations of a constant nature, such as subtracting integers, the time complexity will remain constant.

--- Discount.java
+++ Discount.java
@@ -1,3 +1,29 @@
+Problem
+Alice buys a toy with a selling price of 100100 rupees. There is a discount of xx percent on the toy. Find the amount Alice needs to pay for it.
+
+Input Format
+The first line of input will contain a single integer TT, denoting the number of test cases.
+The first and only line of each test case contains a single integer, xx — the discount on the toy.
+Output Format
+For each test case, output on a new line the price that Alice needs to pay.
+
+Constraints
+1 \leq T \leq 1001≤T≤100
+0 \leq x \lt 1000≤x<100
+
+
+Sample 1:
+Input           |   Output         
+4               |    95
+5               |    91
+9               |    89
+11              |    79
+21              |
+
+Explanation:
+Test case 11: The discount is 55 percent, i.e. 55 rupees. So, Alice will have to pay 100-5=95100−5=95 rupees.
+
+
 /* package codechef; // don't place package name! */
 
 import java.util.*;
